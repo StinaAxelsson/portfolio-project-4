9547503f5c0da3a29c9f39eeeee82138d5e4cbfd
StinaAxelsson <stina91@live.se>
2021-12-27 10:43:23 +0000

    add commentadmin in admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('username', 'status', 'created_on')
+    search_fields = ['username', 'content']
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('post', 'created_on')
+    search_fields = ('name', 'body')
--- utopially/settings.py
+++ utopially/settings.py
@@ -33,8 +33,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['utopially-community.herokuapp.com', 'localhost']
 
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
-    'splashpage',
     'home',
 ]
 
