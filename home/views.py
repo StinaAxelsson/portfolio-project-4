09f11d433ce912d6e9a30400ad1cbd7b06c3384f
StinaAxelsson <stina91@live.se>
2021-12-30 18:47:53 +0000

    Add a form for upload but gonna change that later

--- home/views.py
+++ home/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+from .form import PostUpload
 
 
 class PostListView(View):
 
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
+        form = PostUpload()
 
         context = {
             'home': posts,
+            'form': form,
             
         }
 
