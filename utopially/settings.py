35b9242c692f91e6339e84fbd4c9b00baed594f0
StinaAxelsson <stina91@live.se>
2022-01-04 17:57:16 +0000

    fixed some small errors

--- utopially/settings.py
+++ utopially/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     'crispy_forms',
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -66,7 +67,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- utopially/urls.py
+++ utopially/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
     path('accounts/', include('allauth.urls')),
-] 
+]
