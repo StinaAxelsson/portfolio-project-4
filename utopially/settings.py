aadc0dceb9767072288d2300cd44c1aa7e5896e1
StinaAxelsson <stina91@live.se>
2022-01-04 16:50:09 +0000

    Add styling for ladning page

--- utopially/settings.py
+++ utopially/settings.py
@@ -32,6 +32,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['utopially-community.herokuapp.com', 'localhost']
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Application definition
 
@@ -59,8 +66,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-# LOGIN_REDIRECT_URL = 'home'
-# LOGOUT_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'index'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
