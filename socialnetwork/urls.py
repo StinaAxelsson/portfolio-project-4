488aee758236e2725b16b06392352f7b50217630
StinaAxelsson <stina91@live.se>
2022-01-07 11:01:04 +0000

    Add edit and delete views for posts, and add comments

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetail
+from .views import PostList, PostDetail, PostEdit, PostDelete
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_feed'),
+    path('', PostList.as_view(), name='post_feed'),
     path('post/<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('post/edit/<int:pk>', PostEdit.as_view(), name='post_edit'),
+    path('post/delete/<int:pk>', PostDelete.as_view(), name='post_delete'),
 ]
--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.views import View
 from .models import Post
 from .forms import PostForm, CommentForm
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
-class PostListView(View):
+"""
+Views for the feed, listing all existing posts that been created,
+and a form to create new posts for the feed.
+"""
+class PostList(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all().order_by('-created_on')
         form = PostForm()
@@ -34,6 +40,11 @@ class PostListView(View):
         return render(request, 'post_feed.html', context)
 
 
+"""
+Views for the posts detail, when user click on a post in the feed,
+they see the post on its own and can comment, edit and delete comments,
+or edit and delete the post if its created by the user.
+"""
 class PostDetail(View):
 
     def get(self, request, pk, *args, **kwargs):
@@ -46,3 +57,26 @@ class PostDetail(View):
         }
 
         return render(request, 'post_detail.html', context)
+
+
+"""
+Views for edit a uploaded post, and using get_successs_url to rederict back
+to the post detail template when user has submit the edit.
+"""
+class PostEdit(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'post_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post_detail', kwargs={'pk':pk})
+
+
+"""
+Views for Delete a uploaded post from the feed.
+"""
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_feed')
