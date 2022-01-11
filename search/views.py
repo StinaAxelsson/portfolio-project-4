1e18e7f027e3571d25960e404c93e67e7641baaf
StinaAxelsson <stina91@live.se>
2022-01-11 14:39:33 +0000

    Add views for search but currently not working

--- search/views.py
+++ search/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.views import View
-from socialnetwork import views
+from socialnetwork.models import Users
 
 
 class Search(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'search_users.html')
+        query = self.request.GET.get('query')
+        search_result = Users.objects.filter(
+            Q(user__username__icontains=query)
+        )
+        if not query:
+            search_result = ""
+
+        context = {
+            'search_result': search_result,
+        }
+
+        return render(request, 'search_users.html', context)
