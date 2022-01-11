54b5f15049ec7c9c6a35cc0c9000b49caa61157a
StinaAxelsson <stina91@live.se>
2022-01-11 12:08:53 +0000

    Add following system for users

--- followsystem/models.py
+++ followsystem/models.py
@@ -1,9 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from socialnetwork import models
 
 
-class Followers(models.Model):
-    follower = models.CharField(max_length=1000)
-    user = models.CharField(max_length=1000)
 
-    def __str__(self):
-        return self.user
