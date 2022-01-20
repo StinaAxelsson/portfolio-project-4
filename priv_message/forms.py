18162427a460482b85927d5836f818692984cdf8
StinaAxelsson <stina91@live.se>
2022-01-20 10:53:30 +0000

    fixed errors with send message and add a template for the coversation

--- priv_message/forms.py
+++ priv_message/forms.py
@@ -6,4 +6,11 @@ class InboxForm(forms.Form):
     username = forms.CharField(max_length=100, label='inbox-field')
 
 class MessageForm(forms.Form):
-    message = forms.CharField(label='message-field', max_length=500)
+    message = forms.CharField(
+        label='', 
+        max_length=500,
+        widget=forms.Textarea(attrs={
+            'rows': '3',
+            'placeholder': 'Write a message here...'
+        })
+    )
